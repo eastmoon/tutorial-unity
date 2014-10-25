94af2aaab4ac7f961fffdd8888300220b210af39
Jacky Eastmoon <jacky.eastmoon@gmail.com>
2014-10-25 13:11:35 +0800

    20141025
    0. Startup
    
    Topic on application startup point.
    Reference :
    http://docs.unity3d.com/Manual/ExecutionOrder.html
    http://docs.unity3d.com/Manual/RunningEditorCodeOnLaunch.html

---
+++ 0. Startup/Assets/Source/ApplicationStartup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplicationStartup : MonoBehaviour {
+
+	private bool isUpdate;
+
+	// Use this for initialzation system.
+	void Awake() {
+		Debug.Log ("Application Awake Behaviour");
+	}
+
+	// Use this for initialization GameObject
+	void Start () {
+		Debug.Log ("Application Start Behaviour");
+		this.isUpdate = false;
+
+		//TestClass tc = new TestClass ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!this.isUpdate) 
+		{
+			this.isUpdate = true;
+			Debug.Log ("Application Update Behaviour");
+		}
+	}
+}
